10c8f58952d5075ee951e85b1b1de462a5eddc85
Hannes <Hannes_Herrmann@gmx.net>
2025-08-19 21:04:50 +0200

    added WifiManager

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,12 @@
 #include <ESP8266WiFi.h>
 #endif
 #include <Espalexa.h>
-
-const char* ssid = "";        // Replace with your Wi-Fi SSID
-const char* password = ""; // Replace with your Wi-Fi password
+#include <WiFiManager.h>
 
 Espalexa espalexa; // Create an instance of the Espalexa class
 bool lampState = false; // Current state of the lamp
 const int relayPin = D6;
+const int wifiResetPin = D1;
 
 void lampControl(uint8_t brightness) {
   lampState = brightness == 255;
@@ -25,21 +24,42 @@ void addDevices(){
 }
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+
+  pinMode(wifiResetPin, INPUT);
+
   pinMode(relayPin, OUTPUT);
   digitalWrite(relayPin, LOW); // Ensure the relay is off initially
 
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+
+
+   WiFiManager wm;
+
+  // prüfen ob der Button beim Start gedrückt wird
+  int val = digitalRead(wifiResetPin);   // read the input pin
+
+  if (val == 1) {
+    Serial.println("Button gedrückt -> WLAN-Daten löschen");
+    wm.resetSettings();   // löscht gespeicherte SSID + Passwort
+    ESP.restart();        // Neustart
   }
-  Serial.println("Connected to Wi-Fi");
+
+  // AutoConnect startet AP, wenn keine gültigen Credentials vorhanden sind
+  if (!wm.autoConnect("ESP_PW_12345678", "12345678")) {
+    Serial.println("Keine Verbindung hergestellt, Neustart...");
+    ESP.restart();
+  }
+  Serial.println("Verbunden mit: " + WiFi.SSID());
+
+
 
   addDevices();
+  //espalexa.getDevice(1)->setState(true);
 }
 
 void loop() {
   espalexa.loop(); // Keep the library running
   delay(2);
 }
+
+
