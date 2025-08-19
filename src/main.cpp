24202de04eec1b770af79d4a7357cc1c1a2180cf
Hannes <Hannes_Herrmann@gmx.net>
2025-08-20 00:18:47 +0200

    mic working

--- src/main.cpp
+++ src/main.cpp
@@ -3,35 +3,6 @@
 #ifdef ARDUINO_ARCH_ESP32
   #include <WiFi.h>
   #include <driver/i2s.h>
-
-  // I2S-Konstanten
-  #define SAMPLE_BUFFER_SIZE 512
-  #define SAMPLE_RATE 8000
-  #define I2S_MIC_CHANNEL I2S_CHANNEL_FMT_ONLY_LEFT
-  #define I2S_MIC_SERIAL_CLOCK GPIO_NUM_26
-  #define I2S_MIC_LEFT_RIGHT_CLOCK GPIO_NUM_25
-  #define I2S_MIC_SERIAL_DATA GPIO_NUM_22
-
-  i2s_config_t i2s_config = {
-      .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
-      .sample_rate = SAMPLE_RATE,
-      .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
-      .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
-      .communication_format = (i2s_comm_format_t)(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),
-      .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-      .dma_buf_count = 4,
-      .dma_buf_len = 1024,
-      .use_apll = false,
-      .tx_desc_auto_clear = false,
-      .fixed_mclk = 0};
-
-  i2s_pin_config_t i2s_mic_pins = {
-      .bck_io_num = I2S_MIC_SERIAL_CLOCK,
-      .ws_io_num = I2S_MIC_LEFT_RIGHT_CLOCK,
-      .data_out_num = I2S_PIN_NO_CHANGE,
-      .data_in_num = I2S_MIC_SERIAL_DATA};
-
-  int32_t raw_samples[SAMPLE_BUFFER_SIZE];
 #else
   #include <ESP8266WiFi.h>
 #endif
@@ -42,6 +13,14 @@
 // Pin-Konstanten
 const int RELAY_PIN = 32; // Beispiel: GPIO32 für ESP32
 const int WIFI_RESET_PIN = 17; // Beispiel: GPIO25 für ESP32
+
+// I2S Mikrofon-Pins (INMP441)
+#define I2S_WS 19
+#define I2S_SD 5
+#define I2S_SCK 18
+#define I2S_PORT I2S_NUM_0
+#define bufferLen 64
+int16_t sBuffer[bufferLen];
 
 // Globale Variablen
 Espalexa espalexa;
@@ -61,15 +40,36 @@ void addDevices()
   espalexa.begin();
 }
 
+// I2S Initialisierung
+void i2s_install() {
+  const i2s_config_t i2s_config = {
+    .mode = i2s_mode_t(I2S_MODE_MASTER | I2S_MODE_RX),
+    .sample_rate = 44100,
+    .bits_per_sample = i2s_bits_per_sample_t(16),
+    .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
+    .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_STAND_I2S),
+    .intr_alloc_flags = 0,
+    .dma_buf_count = 8,
+    .dma_buf_len = bufferLen,
+    .use_apll = false
+  };
+  i2s_driver_install(I2S_PORT, &i2s_config, 0, NULL);
+}
+
+void i2s_setpin() {
+  const i2s_pin_config_t pin_config = {
+    .bck_io_num = I2S_SCK,
+    .ws_io_num = I2S_WS,
+    .data_out_num = -1,
+    .data_in_num = I2S_SD
+  };
+  i2s_set_pin(I2S_PORT, &pin_config);
+}
+
 void setup() 
 {
   Serial.begin(115200);
-
-#ifdef ARDUINO_ARCH_ESP32
-  // I2S initialisieren
-  i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
-  i2s_set_pin(I2S_NUM_0, &i2s_mic_pins);
-#endif
+  Serial.println(" ");
 
   pinMode(WIFI_RESET_PIN, INPUT);
   pinMode(RELAY_PIN, OUTPUT);
@@ -93,22 +93,44 @@ void setup()
   Serial.println("Verbunden mit: " + WiFi.SSID());
 
   addDevices();
+
+#ifdef ARDUINO_ARCH_ESP32
+  delay(1000);
+  i2s_install();
+  i2s_setpin();
+  i2s_start(I2S_PORT);
+  delay(500);
+#endif
 }
 
 void loop() 
 {
 #ifdef ARDUINO_ARCH_ESP32
-  // I2S-Mikrofon auslesen und Werte ausgeben
-  size_t bytes_read = 0;
-  i2s_read(I2S_NUM_0, raw_samples, sizeof(int32_t) * SAMPLE_BUFFER_SIZE, &bytes_read, portMAX_DELAY);
-  int samples_read = bytes_read / sizeof(int32_t);
-  for (int i = 0; i < samples_read; i++) {
-    Serial.printf("%ld\n", raw_samples[i]);
+  // Mikrofon-Daten für Serial Plotter
+  int rangelimit = 3000;
+  Serial.print(rangelimit * -1);
+  Serial.print(" ");
+  Serial.print(rangelimit);
+  Serial.print(" ");
+
+  size_t bytesIn = 0;
+  esp_err_t result = i2s_read(I2S_PORT, &sBuffer, bufferLen, &bytesIn, portMAX_DELAY);
+
+  if (result == ESP_OK)
+  {
+    int16_t samples_read = bytesIn / 8;
+    if (samples_read > 0) {
+      float mean = 0;
+      for (int16_t i = 0; i < samples_read; ++i) {
+        mean += (sBuffer[i]);
+      }
+      mean /= samples_read;
+      Serial.println(mean);
+    }
   }
 #endif
 
   espalexa.loop(); // Espalexa am Leben halten
-  delay(2);
 }
 
 
